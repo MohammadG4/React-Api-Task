171fec0f3b4b0b32c0cbcdb1d495abf658233d2a
Mohammad Gafour <mohammadgafourg4@gmail.com>
2025-03-07 03:36:26 +0200

    Add files via upload

--- my-app2/src/components/Form/Form.jsx
+++ my-app2/src/components/Form/Form.jsx
@@ -1,53 +1,102 @@
-import React from 'react'
-import "./Form.css"
-
+import React, { useState } from "react";
+import "./Form.css";
+import axios from "axios";
 
 const Form = () => {
+  const [data, setdata] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    university: "",
+    university_level: "",
+    course: ""
+  });
+
+
+  const submit = (e) => {
+    e.preventDefault(); 
+
+    axios
+      .post("https://fake-form.onrender.com/api/students/addStudent", data)
+      .then((response) => {
+        console.log(response.data);
+        console.log(response.status);
+      })
+      .catch((error) => {
+        console.error("Error submitting form:", error);
+      });
+
+    setdata({
+      name: "",
+      email: "",
+      phone: "",
+      university: "",
+      university_level: "",
+      course: ""
+    });
+  };
+
+
+  const update = (e) => {
+    const { name, value } = e.target;
+    setdata((prevdata) => ({
+      ...prevdata,
+      [name]: value
+    }));
+  };
+
+
+
   return (
     <>
-      <form method='post'>
+      <form onSubmit={submit}>
         <div className="mainForm">
-    <h1 className='cautionH'>Cautions</h1>
-    <p className='cautionP'>Be sure to check your E-mail after submitting the form!</p>
-          <label for="name">Name</label>
-          <input type="text" name="name" id="name" />
+          <h1 className="cautionH">Cautions</h1>
+          <p className="cautionP">
+            Be sure to check your E-mail after submitting the form!
+          </p>
 
-          <label for="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" value={data.name} onChange={update} required />
 
-          <label for="phone">Phone</label>
-          <input type="text" name="phone" id="phone" />
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" value={data.email} onChange={update} required />
 
-          <label for="university">University</label>
-          <select name="university" id="university" required="">
+          <label htmlFor="phone">Phone</label>
+          <input type="text" name="phone" id="phone" value={data.phone} onChange={update} required />
+
+          <label htmlFor="university">University</label>
+          <select name="university" id="university" value={data.university} onChange={update} required>
             <option value="">Select an option:</option>
-            <option value="Cairo ">Cairo </option>
+            <option value="Cairo">Cairo</option>
             <option value="Ain Shams">Ain Shams</option>
             <option value="Helwan">Helwan</option>
             <option value="Other">Other</option>
           </select>
-          <label for="university_level">University Level</label>
-          <select name="university_level" id="university_level" required="">
+
+          <label htmlFor="university_level">University Level</label>
+          <select name="university_level" id="university_level" value={data.university_level} onChange={update} required>
             <option value="">Select an option:</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
-            <option value="4">4</option></select>
-          <label for="data1">Course</label>
-          <select name="data1" id="data1" required="">
+            <option value="4">4</option>
+          </select>
+
+          <label htmlFor="course">Course</label>
+          <select name="course" id="course" value={data.course} onChange={update} required>
             <option value="">Select an option:</option>
             <option value="C++">C++</option>
             <option value="OOP">OOP</option>
             <option value="Flutter">Flutter</option>
-            <option value="DS &amp; Algorithms">DS &amp; Algorithms</option>
+            <option value="DS & Algorithms">DS & Algorithms</option>
             <option value="UI/UX">UI/UX</option>
           </select>
 
-          <button>Submit</button>
-          </div>
+          <button type="submit">Submit</button>
+        </div>
       </form>
     </>
-  )
-}
-
-export default Form
+  );
+};
+export default Form;
--- my-app2/src/components/Nav/Nav.jsx
+++ my-app2/src/components/Nav/Nav.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import "./Nav.css"
-import Logo from "../../assets/logo.png"
+import React, { useState } from 'react';
+import "./Nav.css";
+import Logo from "../../assets/logo.png";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+    console.log("Menu toggled:", !menuOpen); // For debugging
+  };
+
   return (
-        <>
+    <>
       <div className="mainNav">
         <a href="https://180daraga.com/"><img src={Logo} alt="" /></a>
         
-        <ul>
+        <div className="ham" onClick={toggleMenu}><GiHamburgerMenu  color='#66140f' /></div>
+        <ul className={menuOpen ? "mobile-menu-active" : ""}>
           <a href="https://180daraga.com/"><li>Home</li></a>
           <a href="https://180daraga.com/event"><li>Event</li></a>
           <a href="https://180daraga.com/team"><li>Team Hierarchy</li></a>
           <a href="https://180daraga.com/blogs"><li>Blogs</li></a>
-          
         </ul>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
